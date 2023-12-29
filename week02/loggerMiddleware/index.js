5e91bf935b92f8162c99f6e7acf44308669f4b62
Abhishekkumar21 <Abhishek.ak656@gmail.com>
2023-12-30 01:28:19 +0530

    added comments

--- week02/loggerMiddleware/index.js
+++ week02/loggerMiddleware/index.js
@@ -9,18 +9,23 @@ const fs = require("fs");
 const path = require("path");
 const { promisify } = require("util");
 const PORT = process.env.PORT || 3000;
-
 const app = express();
 
+//configuring the logfilepath from root project dir
 const logFilePath = path.join(__dirname, "/data/logResult.txt");
+
+//fetching the directory path name in which logFile is , and checking the directory if it exists if not then
+//creating the directory with name- logFileDirectory
 
 const logFileDirectory = path.dirname(logFilePath);
 if (!fs.existsSync(logFileDirectory)) {
   fs.mkdirSync(logFileDirectory, { recursive: true });
 }
 
+//promifying the fs.appendFile function to get its equivalent function that returns a promise, so that it can be handled easily
 const appendFileAsync = promisify(fs.appendFile);
 
+//middleware
 app.use(async (req, res, next) => {
   const incomingReqTimestamp = new Date().toISOString();
   const incomingReqMethodName = req.method;
@@ -36,6 +41,7 @@ app.use(async (req, res, next) => {
   }
 });
 
+//routes
 app.get("/home", (req, res) => {
   res.send("Hi,there! Welcome to the Home page.");
 });
@@ -44,6 +50,7 @@ app.post("/postroute", (req, res) => {
   res.send("I am from post route");
 });
 
+//starting the server
 app.listen(PORT, () => {
   console.log(`Server is listening at port: ${PORT}`);
 });
