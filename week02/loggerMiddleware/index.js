7a80d894fdbb3757daa4a64a577161d7388b81e3
Abhishekkumar21 <Abhishek.ak656@gmail.com>
2023-12-30 00:59:52 +0530

    added logic for the server app

---
+++ week02/loggerMiddleware/index.js
@@ -0,0 +1,39 @@
+// creating a server-side application having a middleware that will log the timestamp, method and URL of each
+//incoming request being sent to server,
+// in a txt file inside a subdirectory of root project directory
+
+const express = require("express");
+const fs = require("fs");
+const path = require("path");
+const { promisify } = require("util");
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+
+const logFilePath = path.join(__dirname, "/data/logResult.txt");
+
+const logFileDirectory = path.dirname(logFilePath);
+if (!fs.existsSync(logFileDirectory)) {
+  fs.mkdirSync(logFileDirectory, { recursive: true });
+}
+
+const appendFileAsync = promisify(fs.appendFile);
+
+app.use(async (req, res, next) => {
+  const incomingReqTimestamp = new Date().toISOString();
+  const incomingReqMethodName = req.method;
+  const inomingReqURL = req.url;
+  const fileData = `${incomingReqTimestamp} | ${incomingReqMethodName} | ${inomingReqURL}\n`;
+
+  try {
+    await appendFileAsync(logFilePath, fileData);
+    next();
+  } catch (error) {
+    console.log(`Error while writing to file : ${logFilePath}`);
+    next(error);
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is listening at port: ${PORT}`);
+});
