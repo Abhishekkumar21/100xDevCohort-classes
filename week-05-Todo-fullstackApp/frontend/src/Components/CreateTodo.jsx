58c51bec6d62a062f0dc46aea5592ca10f30593e
Abhishekkumar21 <Abhishek.ak656@gmail.com>
2024-01-28 23:28:14 +0530

    added create todos component

--- week-05-Todo-fullstackApp/frontend/src/Components/CreateTodo.jsx
+++ week-05-Todo-fullstackApp/frontend/src/Components/CreateTodo.jsx
@@ -1,10 +1,51 @@
+import { useState } from "react";
+
 export function CreateTodo(){
+    const [title, setTitle] = useState("");
+    const [description, setDescription] = useState("");
+
+    const addTodo = async () => {
+        try {
+            const response = await fetch('http://localhost:8081/addTodos',{
+                method: 'POST',
+                body: JSON.stringify({
+                    title: title,
+                    description: description
+                }),
+                headers: {
+                    "Content-Type": "application/json"
+                }
+               
+            });
+            if(!response.ok){
+                throw new Error(`HTTP error! Status: ${response.status}`)
+            }
+            alert("Todo added");
+        } catch (error) {
+            console.error("Error adding todo:", error.message);
+        }
+    }
+
     return (
         <div>
-            <input type='text' placeholder="title"></input>
-            <input type="text" placeholder="description"></input>
-
-            <button>Add Todo</button>
+            <input id = 'title'
+             type='text' placeholder="title"
+             onChange={function(event){
+                const value = event.target.value;
+                setTitle(value)
+             }}
+             ></input> 
+            <br></br>
+            <input 
+            id="description"
+            type="text" placeholder="description"
+            onChange={function(event){
+                const value = event.target.value;
+                setDescription(value);
+            }}
+            ></input>
+            <br></br>
+            <button onClick ={addTodo}>Add Todo</button>
         </div>
     )
-}
+}   
