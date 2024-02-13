8612f94f46b3859a2b096a7b6a4d63e5afe6b713
abhishekkumar21 <abhishek.ak656@gmail.com>
2024-02-13 16:43:23 +0530

    useEffect hook's concept

--- week-06/week-6.2-reactHooks/src/App.jsx
+++ week-06/week-6.2-reactHooks/src/App.jsx
@@ -1,35 +1,81 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+//useState() -
+// useEffect()-
 
-function App() {
-  const [count, setCount] = useState(0)
+// import { useState, useEffect } from 'react'
+// import axios from 'axios'
+
+// export default function App() {
+//   const [todos, setTodos] = useState([]);
+
+//   useEffect(() => {
+//     axios.get("https://sum-server.100xdevs.com/todos")
+//       .then(response => setTodos(response.data.todos))
+//   }, [])
+
+//   return (
+//     <>
+//       {todos.map((todo) => <Todo title={todo.title} description={todo.description} />)}
+//     </>
+//   )
+// }
+
+// function Todo({ title, description }) {
+//   return (
+//     <div>
+//       <h1> {title}</h1>
+//       <p>{description}</p>
+//     </div>
+//   )
+// }
+
+//-------------------------------------------------------------------------------------------------------------
+//Problem statemet: Write a component that takes a todo id as an input
+// And fetches the data for that todo from the given endpoint
+// And then renders it
+// How would the dependency array change?
+// https://sum-server.100xdevs.com/todo?id=1
+
+import { useState, useEffect } from "react"
+import axios from "axios"
+
+export default function App() {
+  const [btnNumber, setBtnNumber] = useState(1)
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <button onClick={() => setBtnNumber(1)}>1</button>
+      <button onClick={() => setBtnNumber(2)}>2</button>
+      <button onClick={() => setBtnNumber(3)}>3</button>
+      <button onClick={() => setBtnNumber(4)}>4</button>
+      <Todo id={btnNumber} />
+
     </>
   )
 }
 
-export default App
+function Todo({ id }) {
+  const [todo, setTodo] = useState({});
+
+  // useEffect(() => {
+  //   axios.get("https://sum-server.100xdevs.com/todo?" + "id=" + id)
+  //     .then(response => setTodo(response.data.todo))
+  // }, [])   //dependency array is empty, axios api call will made only once when the 'Todo' comp will render for
+  // for the first time
+
+  //passing 'id' in the dependency aaray so that axios api call can be made everytime 'id' changes upon clicking
+  // different buttons
+
+  useEffect(() => {
+    axios.get("https://sum-server.100xdevs.com/todo?" + "id=" + id)
+      .then(response => setTodo(response.data.todo))
+  }, [id])
+
+  return (
+    <>
+      <p>Id : {id}</p>
+      <h2>{todo.title}</h2>
+      <p>{todo.description}</p>
+    </>
+
+  )
+}
